4e1ca78b8b2ecb019f50880f1aaa89b894162d9b
ngkang <skarn2158@naver.com>
2022-10-11 14:51:34 +0900

    feat: Add controller

---
+++ src/main/java/com/de_alone/dokkang/controller/dokkangController.java
@@ -0,0 +1,17 @@
+package com.de_alone.dokkang.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class dokkangController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("hello");
+    }
+
+}
