96c52f7c9264921d37fa1419c15f7e450d29ef88
ngkang <skarn2158@naver.com>
2022-10-12 01:51:57 +0900

    feat: Add login feature

---
+++ src/main/java/com/de_alone/dokkang/payload/response/MessageResponse.java
@@ -0,0 +1,17 @@
+package com.de_alone.dokkang.payload.response;
+
+public class MessageResponse {
+	private String message;
+
+	public MessageResponse(String message) {
+	    this.message = message;
+	  }
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
