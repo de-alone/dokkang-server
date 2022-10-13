f4d248933cb349514f25bae1d870e67891456115
박상준 <cosmoquester@gmail.com>
2022-10-14 00:29:54 +0900

    fix: Permit status api without auth

--- src/main/java/com/de_alone/dokkang/security/WebSecurityConfig.java
+++ src/main/java/com/de_alone/dokkang/security/WebSecurityConfig.java
@@ -61,7 +61,7 @@ public class WebSecurityConfig {
         .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
         .authorizeRequests().antMatchers("/auth/**").permitAll()
-        .antMatchers("/api/test/**").permitAll()
+        .antMatchers("/status").permitAll()
         .anyRequest().authenticated();
 
     http.authenticationProvider(authenticationProvider());
