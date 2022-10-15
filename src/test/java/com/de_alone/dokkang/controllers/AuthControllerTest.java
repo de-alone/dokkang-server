4ad825b4f6a323e567623bccf0be4717feb41236
박상준 <cosmoquester@gmail.com>
2022-10-15 16:18:32 +0900

    fix: Bean Error

--- src/test/java/com/de_alone/dokkang/controllers/AuthControllerTest.java
+++ src/test/java/com/de_alone/dokkang/controllers/AuthControllerTest.java
@@ -3,30 +3,34 @@ package com.de_alone.dokkang.controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.context.ContextConfiguration;
 
-//@RunWith(SpringRunner.class)
-@WebMvcTest(AuthController.class)
+@RunWith(SpringRunner.class)
+@ContextConfiguration
+@AutoConfigureMockMvc
 class AuthControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-
     @Autowired
     private ObjectMapper objectMapper;
-
 
     @DisplayName("Login Test")
     @Test
