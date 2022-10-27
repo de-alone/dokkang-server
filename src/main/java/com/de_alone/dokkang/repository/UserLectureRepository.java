7360d6858fd67f21df420c554b8a1a9a06a68ec0
ngkang <skarn2158@naver.com>
2022-10-27 17:38:45 +0900

    feat: Add lecture, userlecture repo

---
+++ src/main/java/com/de_alone/dokkang/repository/UserLectureRepository.java
@@ -0,0 +1,7 @@
+package com.de_alone.dokkang.repository;
+
+import com.de_alone.dokkang.models.UserLecture;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserLectureRepository extends JpaRepository<UserLecture, Long> {
+}
