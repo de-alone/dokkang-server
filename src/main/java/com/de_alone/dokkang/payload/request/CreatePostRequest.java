f99fd915328128046e7d17bb7b9eb332c1e4d904
ngkang <skarn2158@naver.com>
2022-11-11 02:24:06 +0900

    fix: NotBlank error

--- src/main/java/com/de_alone/dokkang/payload/request/CreatePostRequest.java
+++ src/main/java/com/de_alone/dokkang/payload/request/CreatePostRequest.java
@@ -3,20 +3,11 @@ package com.de_alone.dokkang.payload.request;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
-
 @Getter
 @Setter
 public class CreatePostRequest {
-    @NotBlank
     private Long lecture_id;
-
-    @NotBlank
     private Long user_id;
-
-    @NotBlank
     private String title;
-
-    @NotBlank
     private String content;
 }
