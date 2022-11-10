6791413e207d73e5ad39b4cb99c2804fbdba68e3
ngkang <skarn2158@naver.com>
2022-11-11 01:01:23 +0900

    feat: Add BoardLike entity

---
+++ src/main/java/com/de_alone/dokkang/models/BoardLike.java
@@ -0,0 +1,27 @@
+package com.de_alone.dokkang.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardLike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User userId;
+
+    @ManyToOne
+    @JoinColumn(name = "board_post_id")
+    private BoardPost postId;
+}
