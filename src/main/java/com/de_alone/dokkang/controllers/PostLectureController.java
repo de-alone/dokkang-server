04bc42c4ccc9ce14d69830fbb51e1d77281009eb
박상준 <cosmoquester@gmail.com>
2022-11-12 02:36:58 +0900

    fix: HTTP 규약에 따라 get method에 body 없앰.

--- src/main/java/com/de_alone/dokkang/controllers/PostLectureController.java
+++ src/main/java/com/de_alone/dokkang/controllers/PostLectureController.java
@@ -3,7 +3,6 @@ package com.de_alone.dokkang.controllers;
 import com.de_alone.dokkang.models.BoardComment;
 import com.de_alone.dokkang.models.BoardLike;
 import com.de_alone.dokkang.models.BoardPost;
-import com.de_alone.dokkang.payload.request.PostLectureRequest;
 import com.de_alone.dokkang.payload.response.PostLecture;
 import com.de_alone.dokkang.payload.response.PostLectureResponse;
 import com.de_alone.dokkang.repository.BoardCommentRepository;
@@ -15,12 +14,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -40,11 +39,9 @@ public class PostLectureController {
     BoardLikeRepository boardLikeRepository;
 
     @GetMapping("/{lecture_id}/posts")
-    public ResponseEntity<?> readPostLectureDetail(@RequestParam(required=false) String jwt, @PathVariable Long lecture_id, @Valid @RequestBody PostLectureRequest postLectureRequest) {
-        String before = postLectureRequest.getBefore();
-        Integer limit = postLectureRequest.getLimit();
-
-        LocalDateTime dateTime = before != null ? LocalDateTime.parse(before, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) : LocalDateTime.now();
+    public ResponseEntity<?> readPostLectureDetail(@RequestParam(required=false) String jwt, @PathVariable Long lecture_id, @RequestParam Integer limit, @RequestParam Optional<String> before) {
+        String beforeValue = before.orElse(null);
+        LocalDateTime dateTime = beforeValue != null ? LocalDateTime.parse(beforeValue, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) : LocalDateTime.now();
 
         List<BoardPost> boardPost = boardPostRepository.findAllByLectureId(lectureRepository.findById(lecture_id).orElseThrow(IllegalArgumentException::new));
         Collections.reverse(boardPost);
@@ -59,14 +56,14 @@ public class PostLectureController {
                     List<BoardComment> comment_list = boardCommentRepository.findAllByPostId(post);
                     List<BoardLike> like_list = boardLikeRepository.findAllByPostId(post);
                     posts.add(new PostLecture(post.getId(), post.getLectureId().getId(), post.getTitle(), like_list.size() ,comment_list.size(), post.getCreated_at().toString()));
-                    before = post.getCreated_at().toLocalDateTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+                    beforeValue = post.getCreated_at().toLocalDateTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
                     limit--;
                 }
             }
         }
 
         return ResponseEntity.status(HttpStatus.OK).body(
-                new PostLectureResponse("ok", posts, before));
+                new PostLectureResponse("ok", posts, beforeValue));
     }
 }
 
