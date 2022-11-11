de61a95692235ea0661695925850694931ed4a06
ngkang <skarn2158@naver.com>
2022-11-11 21:08:27 +0900

    feat: Add 'Get Posts of Lecture' api

--- src/main/java/com/de_alone/dokkang/repository/BoardPostRepository.java
+++ src/main/java/com/de_alone/dokkang/repository/BoardPostRepository.java
@@ -1,7 +1,11 @@
 package com.de_alone.dokkang.repository;
 
 import com.de_alone.dokkang.models.BoardPost;
+import com.de_alone.dokkang.models.Lecture;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardPostRepository extends JpaRepository<BoardPost, Long> {
+    List<BoardPost> findAllByLectureId(Lecture lecture);
 }
