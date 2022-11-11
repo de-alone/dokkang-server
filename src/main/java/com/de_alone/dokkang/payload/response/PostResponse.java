8918c53345d924aba0b61766f764e04a17245f3f
ngkang <skarn2158@naver.com>
2022-11-11 12:58:47 +0900

    feat: Add 'Get a Post' api

---
+++ src/main/java/com/de_alone/dokkang/payload/response/PostResponse.java
@@ -0,0 +1,24 @@
+package com.de_alone.dokkang.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class PostResponse {
+    private String status;
+    private Long id;
+    private Long lecture_id;
+    private Long user_id;
+    private String username;
+    private String title;
+    private String content;
+    private String created_at;
+    private Integer num_likes;
+    private List<Comment> comments;
+}
+
