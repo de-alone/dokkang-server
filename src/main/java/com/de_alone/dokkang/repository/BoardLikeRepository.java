13b5a39276a1fce89187d53ebe5cf0e0fa79cfef
박상준 <cosmoquester@gmail.com>
2022-11-13 15:46:33 +0900

    perf: Improve to check already liked

--- src/main/java/com/de_alone/dokkang/repository/BoardLikeRepository.java
+++ src/main/java/com/de_alone/dokkang/repository/BoardLikeRepository.java
@@ -2,6 +2,8 @@ package com.de_alone.dokkang.repository;
 
 import com.de_alone.dokkang.models.BoardLike;
 import com.de_alone.dokkang.models.BoardPost;
+import com.de_alone.dokkang.models.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +12,6 @@ public interface BoardLikeRepository extends JpaRepository<BoardLike, Long> {
     List<BoardLike> findAllByPostId(BoardPost boardPost);
 
     Boolean existsByPostId(BoardPost boardPost);
+
+    List<BoardLike> findByPostIdAndUserId(BoardPost postId, User userId);
 }
