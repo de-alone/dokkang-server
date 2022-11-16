7ba3a458d5de1fed332f014a7ea3db447919b893
ngkang <skarn2158@naver.com>
2022-11-16 12:36:41 +0900

    feat: Add StudyGroup Repositories

---
+++ src/main/java/com/de_alone/dokkang/repository/StudyGroupCommentRepository.java
@@ -0,0 +1,11 @@
+package com.de_alone.dokkang.repository;
+
+import com.de_alone.dokkang.models.StudyGroupComment;
+import com.de_alone.dokkang.models.StudyGroupPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface StudyGroupCommentRepository extends JpaRepository<StudyGroupComment, Long> {
+    List<StudyGroupComment> findAllByPostId(StudyGroupPost studyGroupPost);
+}
