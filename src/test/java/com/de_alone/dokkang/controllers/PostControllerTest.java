dad4bbc1d2c4a969ed3741c606d5d6b0cac1572e
JeongHanLim <jhlim271@naver.com>
2022-11-20 13:55:28 +0900

    test: comment/commentstudy/lecture/lecturespecific/participate/post/studygroup Fin.

--- src/test/java/com/de_alone/dokkang/controllers/PostControllerTest.java
+++ src/test/java/com/de_alone/dokkang/controllers/PostControllerTest.java
@@ -1,12 +1,15 @@
 package com.de_alone.dokkang.controllers;
 
 import com.de_alone.dokkang.models.*;
+import com.de_alone.dokkang.payload.request.CreatePostRequest;
 import com.de_alone.dokkang.repository.*;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -25,9 +28,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.de_alone.dokkang.DokkangServerApplication;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @RunWith(SpringRunner.class)
@@ -35,72 +40,90 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 @AutoConfigureMockMvc
 @SpringBootTest(classes = DokkangServerApplication.class)
 class PostControllerTest {
-
     @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    LectureRepository lectureRepository;
+    MockMvc mockMvc;
 
     @MockBean
     BoardPostRepository boardPostRepository;
 
     @MockBean
-    BoardCommentRepository boardCommentRepository;
+    LectureRepository lectureRepository;
+
+    @MockBean
+    UserRepository userRepository;
 
     @MockBean
     BoardLikeRepository boardLikeRepository;
-    Long lecture_id = 101L;
-    Lecture lecture = new Lecture(lecture_id, "SWE3033", "Software Engineering", "Cha");
 
-    User user1 = new User(1L, "username", "email@email.com", "password");
-    User user2 = new User(2L, "user", "email@email.com", "password");
+    @MockBean
+    BoardCommentRepository boardCommentRepository;
 
-    BoardPost boardPost1 = new BoardPost(1L, lecture, user1 , "test of board1", "content");
+    Lecture lecture = new Lecture(101L, "SWE3033", "Intro SE", "Cha");
+    User user = new User(1L, "user", "email", "username");
+    User user2 = new User(2L, "username", "email", "username");
 
-    BoardPost boardPost2 = new BoardPost(2L, lecture, user2, "test of board2","content");
+    CreatePostRequest createPostRequest = new CreatePostRequest(
+            lecture.getId(),
+            user.getId(),
+            "title",
+            "content"
+    );
+    BoardPost boardPost = new BoardPost(
+            1000L,
+            lecture,
+            user,
+            createPostRequest.getTitle(),
+            createPostRequest.getContent()
+    );
 
-
-    BoardComment boardComment1
-            = new BoardComment(boardPost1, user1, new Date(), "content" );
-    BoardComment boardComment2
-            = new BoardComment(boardPost1, user1, new Date(), "content");
-
-    BoardLike boardLike
-            = new BoardLike(1L, user2, boardPost1);
-    BoardLike boardLike2
-            = new BoardLike(2L, user1, boardPost2);
-    @DisplayName("get Post Lecture Detail")
+    @DisplayName("RegisterPost")
     @Test
-    @WithMockUser(username="username", password="password")
-    public void testReadPostLectureDetail_yesBefore_NotYetDeveloped() throws Exception {
-        boardPost1.setCreated_at(Timestamp.valueOf(LocalDateTime.now()));
-        boardPost2.setCreated_at(Timestamp.valueOf(LocalDateTime.now()));
-
-        List<BoardPost> listOfBoardPost = new ArrayList<>();
-        listOfBoardPost.add(boardPost1);
-        listOfBoardPost.add(boardPost2);
-
-        given(lectureRepository.findById(lecture_id)).willReturn(Optional.of(lecture));
-        given(boardPostRepository.findAllByLectureId(lecture)).willReturn(listOfBoardPost);
-
-        when(boardCommentRepository.findAllByPostId(boardPost1)).thenReturn(List.of(boardComment1, boardComment2));
-        when(boardCommentRepository.findAllByPostId(boardPost2)).thenReturn(List.of());
-
-        when(boardLikeRepository.findAllByPostId(boardPost1)).thenReturn(List.of(boardLike));
-        when(boardLikeRepository.findAllByPostId(boardPost2)).thenReturn(List.of(boardLike2));
+    @WithMockUser(username = "username", password = "password")
+    public void testRegisterPost() throws Exception {
 
 
+        given(lectureRepository.findById(lecture.getId())).willReturn(Optional.of(lecture));
+        given(userRepository.findById(user.getId())).willReturn(Optional.of(user));
 
+        given(boardPostRepository.save(any(BoardPost.class))).willReturn(boardPost);
 
-        RequestBuilder request = MockMvcRequestBuilders.get("/lecture/101/posts")
-                .param("limit", String.valueOf(20))
-                .param("before", "2022-12-22 12:34:56");
+        HashMap<String, Object> input = new HashMap<>();
+        input.put("user_id", createPostRequest.getUser_id());
+        input.put("lecture_id", createPostRequest.getLecture_id());
+        input.put("title", createPostRequest.getTitle());
+        input.put("content", createPostRequest.getContent());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        RequestBuilder request = MockMvcRequestBuilders.post("/post")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(input));
 
         mockMvc.perform(request)
+                .andExpect(status().is2xxSuccessful())
                 .andDo(print());
+
     }
 
+    @DisplayName("readPostDetail")
+    @Test
+    @WithMockUser(username = "username", password = "password")
+    public void testReadPostDetail() throws Exception {
+
+        given(boardPostRepository.findById(boardPost.getId())).willReturn(Optional.of(boardPost));
+        given(boardLikeRepository.findAllByPostId(boardPost)).willReturn(List.of(
+                new BoardLike(1000L, user, boardPost),
+                new BoardLike(2000L, user2, boardPost)));
+        given(boardCommentRepository.findAllByPostId(boardPost)).willReturn(List.of(
+                new BoardComment(5L, user, boardPost, Timestamp.valueOf(LocalDateTime.now()), "d"),
+                new BoardComment(6L, user2, boardPost, Timestamp.valueOf(LocalDateTime.now()), "e")
+        ));
 
 
+        RequestBuilder request = MockMvcRequestBuilders.get("/post/{id}", boardPost.getId());
+
+        mockMvc.perform(request)
+                .andExpect(status().is2xxSuccessful())
+                .andDo(print());
+    }
 }
